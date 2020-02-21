b693e42454c92eef7dc1211d02f499994972fa3a
Gabby Sanghera <gsanghera@uk-c02zw0h2lvdm.uk.deloitte.com>
2020-02-21 11:24:31 +0000

    orginal

--- client/src/components/dashboard/OrderItem.js
+++ client/src/components/dashboard/OrderItem.js
@@ -4,7 +4,6 @@ import Moment from "react-moment";
 import { connect } from "react-redux";
 import { addLike, removeLike } from "../../actions/course";
 import { approveOrder } from "../../actions/order";
-import { deleteOrder } from "../../actions/order";
 import Chip from "@material-ui/core/Chip";
 import Button from "@material-ui/core/Button";
 
@@ -21,17 +20,13 @@ const leverageStyles = makeStyles({
   },
   approveButton: {
     margin: "0 1rem"
-  },
-  deleteButton: {
-    margin: "0 1rem"
   }
 });
 
 const OrderItem = ({
   order: { course, date, approved, user, _id },
   isAdmin,
-  approveOrder,
-  deleteOrder
+  approveOrder
 }) => {
   const classes = leverageStyles();
   return (
@@ -54,51 +49,17 @@ const OrderItem = ({
           </CardContent>
         </CardActionArea>
         <CardContent>
-        
           {isAdmin && (
             <Typography variant="subtitle1" paragraph color="textSecondary">
               Applied by: {user.name}
             </Typography>
           )}
-            {approved ? (
-            <div>
-            <Grid container spacing={1} justify= "flex-start">
-            <Grid item>
-          <Chip color="primary" label="Approved" />
-          </Grid>
-          <Grid item>
-          <Button
-          variant="contained"
-          color="secondary"
-          className={classes.deleteButton}
-          onClick={() => deleteOrder(_id)}
-        >
-          Delete 
-        </Button>
-        </Grid>
-        </Grid>
-          </div>) : (<div>
-            <Grid container spacing={2} justify= "flex-start">
-            <Grid item>
-          <Chip label="Pending" />
-          </Grid>
-          <Grid item>
-          <Button
-          variant="contained"
-          color="secondary"
-          className={classes.deleteButton}
-          onClick={() => deleteOrder(_id)}
-        >
-          Delete 
-        </Button>
-        </Grid>
-        </Grid>
-          </div>
-
+          {approved ? (
+            <Chip color="primary" label="Approved" />
+          ) : (
+            <Chip label="Pending" />
           )}
-
           {isAdmin && !approved && (
-            
             <Button
               variant="outlined"
               color="primary"
@@ -107,9 +68,6 @@ const OrderItem = ({
             >
               Approve
             </Button>
-            
-          
-
           )}
         </CardContent>
       </Card>
@@ -125,14 +83,13 @@ OrderItem.propTypes = {
   order: PropTypes.object.isRequired,
   addLike: PropTypes.func.isRequired,
   approveOrder: PropTypes.func.isRequired,
-  removeLike: PropTypes.func.isRequired,
-  deleteOrder: PropTypes.func.isRequired
+  removeLike: PropTypes.func.isRequired
 };
 
 const mapStateToProps = state => ({
   auth: state.auth
 });
 
-export default connect(mapStateToProps, { addLike, removeLike, approveOrder, deleteOrder })(
+export default connect(mapStateToProps, { addLike, removeLike, approveOrder })(
   OrderItem
 );
